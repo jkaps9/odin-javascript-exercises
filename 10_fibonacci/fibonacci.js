ecbd33f665a392c71c0572a7a0f066f4997deaa9
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-03 23:24:09 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
 
+    if (num < 0) {
+        return "OOPS"
+    }
+
+    //1, 1, 2, 3, 5, 8
+    let fibArr = [0, 1]
+
+    for (i = 2; i <= num; i++) {
+        fibArr[i] = fibArr[i - 2] + fibArr[i - 1]
+    }
+
+    return fibArr[num]
 };
 
 // Do not edit below this line
