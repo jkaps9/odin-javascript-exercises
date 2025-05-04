33b5dcdab7030716aa1160ead27929e5760ae8ea
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-03 23:34:27 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+    people.sort((a, b) => {
+        const currentYear = (new Date()).getFullYear()
+        const aYOD = a.yearOfDeath === undefined ? currentYear : a.yearOfDeath
+        const bYOD = b.yearOfDeath === undefined ? currentYear : b.yearOfDeath
+        if ((aYOD - a.yearOfBirth) < (bYOD - b.yearOfBirth)) {
+            return 1
+        } else {
+            return -1
+        }
+    }
+    )
+    return people[0]
 };
 
 // Do not edit below this line
