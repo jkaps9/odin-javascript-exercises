814180a71ecb7d0bf80a799284e62950e1ab5aef
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-03 23:15:49 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const re = /[a-zA-Z1-9]/
+    word = word.toLowerCase()
+    const backwards = word.split('').reverse().filter(letter => re.test(letter)).join('')
+    const forwards = word.split('').filter(letter => re.test(letter)).join('')
+    return forwards === backwards
 };
 
 // Do not edit below this line
